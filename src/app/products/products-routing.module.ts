a20c846254629e59543e01f3f1de2867e3ef5fe8
kenier <kenier202@gmail.com>
2024-11-29 10:27:13 -0500

    feat(routes): add basics routes

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'products',
+        component: ProductPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'products'
+      },
+
+    ]
+
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
